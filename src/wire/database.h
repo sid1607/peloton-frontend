94f17d1920e2786bfb403653d8151d8b6f776d05
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-05-11 05:51:29 -0400

    Write_buffer header flushing check bug fix

---
+++ src/wire/database.h
@@ -0,0 +1,35 @@
+//
+// Created by Zrs_y on 4/18/16.
+//
+
+#ifndef PELOTON_DATABASE_H
+#define PELOTON_DATABASE_H
+
+#include <vector>
+#include <string>
+#include <tuple>
+#include "logger.h"
+#include "wire.h"
+
+namespace peloton {
+namespace wiredb {
+typedef std::pair<std::vector<unsigned char>, std::vector<unsigned char>> ResType;
+// fieldinfotype: field name, oid (data type), size
+typedef std::tuple<std::string, int, int> FieldInfoType;
+
+#define WIRE_INTEGER 1
+#define WIRE_TEXT 2
+#define WIRE_FLOAT 3
+#define WIRE_NULL 4
+
+class DataBase {
+public:
+  DataBase() { }
+
+  virtual ~DataBase() { }
+};
+
+}
+}
+
+#endif //PELOTON_DATABASE_H
